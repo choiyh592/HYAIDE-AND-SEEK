6c3fe434399ed032339212c5a737cb108928a875
choiyh592 <choiyh592@gmail.com>
2026-02-25 09:47:20 +0900

    Create model factory

--- models/model_factory.py
+++ models/model_factory.py
@@ -1,5 +1,14 @@
+from .SANA import SANA
+from .InstaFlow import InstaFlow
+# TODO : Add imports
+
 def create_model(args):
-    pass
+    model_name = args.model.lower()
+
+    if model_name == "sana":
+        return SANA()
+    elif model_name == "instaflow":
+        return InstaFlow()
 
 
 
